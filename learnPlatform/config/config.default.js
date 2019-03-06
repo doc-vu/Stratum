9b40976ff0a24765ebac793d9f6a842053747317
root <anirban.bhattacharjee@vanderbilt.edu>
2019-03-06 00:37:21 +0000

    Stratum

---
+++ learnPlatform/config/config.default.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var config = require('./config.webgme'),
+    validateConfig = require('webgme/config/validator');
+
+// Add/overwrite any additional settings here
+// config.server.port = 8080;
+// config.mongo.uri = 'mongodb://127.0.0.1:27017/webgme_my_app';
+
+// Authentication/Authorization
+config.authentication.enable = true;
+
+//  Setting Preferences
+config.authentication.allowGuests = true;
+config.authentication.guestAccount = 'guest';
+config.authentication.allowUserRegistration = true;
+config.authentication.guestAccount = 'guest'
+
+// Routing
+config.authentication.logInUrl = '/profile/login';
+config.authentication.logOutUrl = '/profile/login';
+
+// Plugins
+config.plugin.allowServerExecution = true;
+// Custom constraints
+config.core.enableCustomConstraints = true;
+// WebHooks
+config.webhooks.enable = true;
+// UI
+//config.visualization.layout.default = 'SidebarLayout';
+config.executor.enable = true;
+config.executor.clearOldDataAtStartUp = true;
+config.visualization.svgDirs = ['/home/ubuntu/streamlinedMLProject/learnPlatform/icons'];
+// RequireJS paths
+config.requirejsPaths.cloudcamp = "./src/common/"
+
+validateConfig(config);
+module.exports = config;
